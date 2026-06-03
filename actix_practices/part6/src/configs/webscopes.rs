47ed7da095f4ca3c1cf2214d1443ca01fa87a9e0
ironwill01 <ironwillgit01@gmail.com>
2026-06-04 01:46:48 +0330

    Added some URL dispatch stuff into part6 its not finished tho

--- actix_practices/part6/src/configs/webscopes.rs
+++ actix_practices/part6/src/configs/webscopes.rs
@@ -1,6 +1,9 @@
 use {
     actix_web::{
-        HttpResponse, Responder, get,
+        HttpRequest, HttpResponse, Responder, Result,
+        body::BoxBody,
+        get, guard,
+        http::header::{self, ContentType},
         post,
         web::{self},
     },
@@ -10,16 +13,22 @@ use {
 };
 
 pub mod scopes {
+
     use super::*;
-    pub trait DataBase 
-    where Self : Default
+    pub trait DataBase
+    where
+        Self: Default,
     {
         fn new() -> web::Data<Self> {
             web::Data::new(Self::default())
         }
     }
 
-    pub fn simple_index(cfg: &mut web::ServiceConfig, users_and_request: web::Data<UserAppState> , userdatabase : web::Data<UserDataBase>) {
+    pub fn simple_index(
+        cfg: &mut web::ServiceConfig,
+        users_and_request: web::Data<UserAppState>,
+        userdatabase: web::Data<UserDataBase>,
+    ) {
         cfg.service(
             web::scope("/simple")
                 .app_data(users_and_request)
@@ -28,6 +37,15 @@ pub mod scopes {
                 .service(get_users_json)
                 .service(setusers)
                 .service(getusers)
+                .service(index)
+                .service(url_index)
+                // this one is actually for URL dispatch using it for `url_for`
+                .service(
+                    web::resource("/get_users/{a}/{b}/")
+                        .name("blud")
+                        .guard(guard::Get())
+                        .to(HttpResponse::Ok),
+                ),
         );
     }
 
@@ -98,7 +116,7 @@ pub mod scopes {
         };
     }
 
-    #[derive(Deserialize , Default)]
+    #[derive(Deserialize, Default)]
     struct UserInfo {
         #[serde(rename = "User.name")]
         name: String,
@@ -106,13 +124,40 @@ pub mod scopes {
         password: String,
     }
 
-    #[derive(Deserialize , Default)]
+    #[derive(Deserialize, Default)]
     pub struct UserDataBase {
         users: Mutex<Vec<UserInfo>>,
     }
 
     impl DataBase for UserDataBase {}
 
+    // i still found no use here even tho i impl respond_to there is no point ( literally )
+    impl Responder for UserDataBase {
+        type Body = BoxBody;
+        fn respond_to(self, _req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+            match self.users.lock() {
+                Ok(users) => {
+                    let body = users
+                        .iter()
+                        .map(|u| format!("{} : {}", u.name, u.password))
+                        .collect::<Vec<_>>()
+                        .join("\n");
+
+                    HttpResponse::Ok()
+                        .insert_header(ContentType::html())
+                        .body(body)
+                }
+                Err(err) => {
+                    eprintln!("Mutex poisoned : {}", err);
+                    HttpResponse::InternalServerError().body("Internal server error !")
+                }
+            }
+        }
+    }
+
+    // Also for URL dispatch ( which we didnt do here ) you need to either
+    // use match_info from HttpRequset or make an struct and use it with path
+    // which you need to use Deserialize from serde so you can actually do it
     #[get("/set_user")]
     async fn setusers(
         query: web::Query<UserInfo>,
@@ -128,12 +173,11 @@ pub mod scopes {
                 return HttpResponse::InternalServerError().body("Internal server error!");
             }
         }
-
         HttpResponse::Ok().body("User added to the database !")
     }
 
-    #[get("/get_users")]
-    async fn getusers(data_base: web::Data<UserDataBase>) -> impl Responder {
+    #[get("/get_users/{name}/{lastname}")]
+    async fn getusers(data_base: web::Data<UserDataBase> , path : web::Path<(String , String)>) -> impl Responder {
         let mut body = String::new();
         match data_base.users.lock() {
             Ok(users) => {
@@ -149,6 +193,24 @@ pub mod scopes {
                 return HttpResponse::InternalServerError().body("Internal server error!");
             }
         }
-        HttpResponse::Ok().body(body)
+        let (firstname , lastname) = path.into_inner();
+        HttpResponse::Ok().body(format!("hey {} {}\n{}" , firstname , lastname , body))
+    }
+
+    // Url syntax and the how HttpRequset can extract information from URL using `match_info()`
+    #[get("/a/{v1}/{v2}/")]
+    async fn index(req: HttpRequest) -> Result<String> {
+        let v1: u8 = req.match_info().get("v1").unwrap().parse().unwrap();
+        let v2: u8 = req.match_info().query("v2").parse().unwrap();
+        let (v3, v4): (u8, u8) = req.match_info().load().unwrap();
+        Ok(format!("Values {} {} {} {}", v1, v2, v3, v4))
+    }
+
+    #[get("/test/")]
+    async fn url_index(req: HttpRequest) -> Result<HttpResponse> {
+        let url = req.url_for("blud", ["nikan", "sadeghi"])?;
+        Ok(HttpResponse::Found()
+            .insert_header((header::LOCATION, url.as_str()))
+            .finish())
     }
 }
